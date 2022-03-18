f981307a23ed5fcd09ee5fe02499e7f87093991d
puritybirir <puritybirir@gmail.com>
2022-03-18 15:17:58 +0300

    Variables and Datatypes

---
+++ intro/var1.js
@@ -0,0 +1,26 @@
+// tell the computer to remember something
+
+// Declaring while assigning
+var firstName = "Steve"; // Datatype string
+var id = 234234; // Datatype number
+var alive = true; // Datatype boolean
+
+
+// Re-assigning
+console.log(firstName);
+console.log(id);
+console.log(alive);
+
+alive = false;
+id = 7;
+firstName = "John"
+
+console.log(firstName);
+console.log(id);
+console.log(alive);
+
+//Declaring without assigning a variable
+var xyz;
+
+
+
---
+++ intro/var2.js
@@ -0,0 +1,27 @@
+var firstName = "Steve";
+var id  = 234234;
+var alive = true;
+var fiveDs;
+
+firstName;
+
+firstName = "Ricky"
+firstName;
+
+// Concatenation
+firstName = firstName + " Bobby";
+firstName;
+
+id = 5+3;
+id;
+
+id = id-2;
+id;
+
+// Complex Data Types
+// Array
+fiveDs = ["Dodge", "Duck", "Dip", "Dive", "Dodge"];
+console.log(fiveDs);
+console.log(fiveDs.length);
+
+console.log(fiveDs[0] + " " + fiveDs[3])
