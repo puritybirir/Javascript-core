d6e0f142cf798c1af820d46d46b94a07ee8e1cf3
puritybirir <puritybirir@gmail.com>
2022-03-18 15:24:24 +0300

    If statements and falsey

---
+++ loops/if.js
@@ -0,0 +1,20 @@
+var first_name = "Ricky";
+var last_name = "Bobby";
+var age = 45;
+var first = true;
+var last = false;
+
+if ( age == 45 ){
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+//Falsey examples of falsey below:
+// False, null, undefinded, 0, "empty string", 'single empty string', not a number
+
+if(last){
+  console.log("falsey")
+} else {
+  console.log("falsey")
+}
