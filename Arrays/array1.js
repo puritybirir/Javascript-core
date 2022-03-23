acd0af7385f5e93eca011ed7d43b83c7199b46a5
puritybirir <puritybirir@gmail.com>
2022-03-23 15:55:05 +0300

    Basics of Javascript arrays

---
+++ Arrays/array1.js
@@ -0,0 +1,35 @@
+var characters = ['Luke', 'Leia', 'Han', 'Chewie'];
+
+console.log(characters);
+console.log(characters.length)
+console.log(characters[0]);
+
+var last_item = characters[characters.length - 1];
+console.log(last_item);
+
+// Adding to an array
+
+characters.push('Anakin');
+characters.push('Obiwan');
+console.log(characters);
+
+console.log(last_item);
+
+//Removing from an array
+characters.pop();
+characters.pop()
+characters
+
+//Adding from the front
+characters.unshift('C3PO')
+characters.unshift('R2D2')
+characters
+
+// Removing first item
+characters.shift();
+characters;
+
+console.log(characters.shift())
+
+
+
