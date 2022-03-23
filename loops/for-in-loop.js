b26dd955ac8da268190396f6747ecd1317ac4534
puritybirir <puritybirir@gmail.com>
2022-03-23 17:41:25 +0300

    Looping through objects using for-in loop in js

---
+++ loops/for-in-loop.js
@@ -0,0 +1,11 @@
+var monsters = {
+  Canada: "Sasquatch",
+  Nepal: "Yeti",
+  Scotland: "Loch Ness Monster",
+};
+
+for( let prop in monsters) {
+  // console.log(prop);
+  // console.log(monsters[prop])
+  console.log("in", prop, "we have the", monsters[prop])
+}
