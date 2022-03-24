d432382b38a9621158e4f59dfa69e1c612d632b1
puritybirir <puritybirir@gmail.com>
2022-03-24 14:59:35 +0300

    While do and do while loops

---
+++ loops/while-do.js
@@ -0,0 +1,13 @@
+let total = 0;
+
+// while loop
+// while (total<30){
+//   total += Math.floor(Math.random() * 5) +1;
+//   console.log(total);
+// }
+
+//do while
+do {
+  total += Math.floor(Math.random() * 5) +1;
+  console.log(total);
+} while (total<30);
