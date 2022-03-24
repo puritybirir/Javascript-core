1187afb5b4084caef1bc33297a99894598c7b73f
puritybirir <puritybirir@gmail.com>
2022-03-24 15:45:48 +0300

    Add return statements" "

--- Functions/functions1.js
+++ Functions/functions1.js
@@ -6,35 +6,51 @@ var pop_cost = 3.00;
 //users money
 var account_balance = 15.75;
 
+//check account balance
+function checkBalance(amount){
+  if(account_balance - amount > 0) {
+    // Transaction allowed
+    return true;
+  } else {
+    // Transaction not allowed
+    console.log("INSUFFICIENT FUNDS");
+    return false;
+  }
+}
+
 //drink beer
 function drink_beer(){
-  account_balance = account_balance - beer_cost;
-  console.log(account_balance);
+  if(checkBalance(beer_cost)){
+    account_balance = account_balance - beer_cost;
+    console.log("Enjoy your beer");
+  }
+  console.log('\tBalance:', account_balance.toFixed(2))
 }
 
 //eat burger
 function eat_burger(){
-  account_balance = account_balance - burger_cost;
-  console.log(account_balance);
+  if(checkBalance(burger_cost)){
+    account_balance = account_balance - burger_cost;
+    console.log("Enjoy your burger");
+  }
+  console.log('Balance', account_balance.toFixed(2));
 }
 
 //drink pop
 function drink_pop(){
-  account_balance = account_balance - pop_cost;
-  console.log(account_balance);
+  if(checkBalance(drink_pop)){
+    account_balance = account_balance - pop_cost;
+    console.log("Enjoy your pop");
+  }
+  console.log('Balance', account_balance.toFixed(2));
 }
 
-// Visit the carnival eat and drink
-var meal_list = [drink_beer, drink_pop, eat_burger, drink_beer];
-
-function visitCarnival(mList, msg){
+function visitCarnival(mList){
   for (var i=0, num=mList.length; i<num; i++){
     mList[i]();
-    msg;
-    // mList[i].call();
   }
 }
 
-visitCarnival(meal_list, "Hello")
+visitCarnival([eat_burger, drink_beer, eat_burger, drink_beer])
 
 
