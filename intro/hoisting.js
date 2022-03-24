eed4a5153cca4b5413b68ee364b1606f8cb657fa
puritybirir <puritybirir@gmail.com>
2022-03-24 17:25:32 +0300

    Hoisting in Javascript

---
+++ intro/hoisting.js
@@ -0,0 +1,15 @@
+// hoisting
+// the process of taking two passes through the js file_exists
+// firs pass hoists all the declarations to the top of the file
+
+var a;
+var b = 4;
+
+function c() {
+  // function declaration
+}
+
+var d = 5;
+var e = function () {
+  //function expression
+}
