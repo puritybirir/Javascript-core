19e59e24e981efc14bd0eb54c57c156d46207ce8
puritybirir <puritybirir@gmail.com>
2022-04-06 15:10:13 +0300

    Numbers in Javascript

---
+++ numbers.js
@@ -0,0 +1,34 @@
+// Globals and Numbers and Math.
+// Practice the Jaascript math method
+
+var r = parseInt("010101010101111", 2);
+console.log(r);
+
+let num = 234.23423423;
+let str = "234.23423423";
+r = Number.parseInt(num);
+let x = Number.parseInt(str, 10);
+
+console.log(r);
+console.log(x);
+
+let p = num.toFixed(4);
+console.log(p);
+
+
+// Math objectt
+
+let f = Math.round(num);
+let g = Math.floor(num);
+let l = Math.random();
+
+console.log(f);
+console.log(g);
+console.log(l);
+
+let q = Math.max(97, 77, 7, 37, 47, 57);
+console.log(q);
+
+
+
+
