eee60b82b70b3b777eb326bfe7f1639c68e24102
puritybirir <puritybirir@gmail.com>
2022-04-29 17:35:33 +0300

    Array Splice

---
+++ Array2/splice.js
@@ -0,0 +1,12 @@
+// splice changes the original array
+
+const numbers = [1, 2, 3, 4, 5];
+
+const deleted = numbers. splice (2, 0, 6, 7, 10, 100);
+
+console.log(numbers);
+console.log(deleted);
+
+
+
+
