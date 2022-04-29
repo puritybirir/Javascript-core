33d62802a7dc09641465112dc8c084127c213c1d
puritybirir <puritybirir@gmail.com>
2022-04-29 17:29:53 +0300

    Array slice

---
+++ Array2/slice.js
@@ -0,0 +1,13 @@
+const numbers = [1, 2, 3, 4, 5];
+
+const numbers2 = numbers.slice(1, 4);
+
+console.log(numbers2);
+
+
+const participants = ['Florin', 'Ivan', 'Liam', 'Jai', 'Patrik'];
+
+const winners = participants.slice(0, 3);
+console.log(winners);
+
+
