fefc2598d22ac5a16070265142b40ebd0fa5bac4
puritybirir <puritybirir@gmail.com>
2022-04-30 16:22:49 +0300

    Reversing an array

---
+++ Array2/reverse.js
@@ -0,0 +1,16 @@
+const numbers = [1, 2, 3, 4, 5];
+
+const newArr = [...numbers].reverse();
+
+console.log(newArr);
+
+// Reverse the characters in a string
+
+const str = 'Coding is fun!';
+
+const res = str.split('').reverse().join('');
+
+console.log(res);
+
+
+
