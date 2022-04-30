f323a308800f217be7e2ec610ccce1003cb0e3b4
puritybirir <puritybirir@gmail.com>
2022-04-30 16:27:44 +0300

    Pop and push

---
+++ Array2/pop.js
@@ -0,0 +1,8 @@
+// Removes last item from an array and returns it
+
+const numbers = [1, 2, 3];
+const lastItem = numbers.pop();
+
+console.log(numbers, lastItem);
+
+
---
+++ Array2/push.js
@@ -0,0 +1,7 @@
+const numbers = [1, 2, 3];
+
+const total = numbers.push(4, 5, 6, 7, 8);
+
+console.log(total);
+
+
