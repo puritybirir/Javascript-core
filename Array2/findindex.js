8c080fe71ef05a8b8cd5004e06db3962f404f94d
puritybirir <puritybirir@gmail.com>
2022-04-30 22:18:56 +0300

    Find index method

---
+++ Array2/findindex.js
@@ -0,0 +1,11 @@
+// Finds the element and returns its index
+
+const numbers = [1, 2, 3, 4, 5]
+
+const res = numbers.findIndex(findThree);
+
+function findThree (value) {
+  return value === 3;
+}
+
+console.log(res);
